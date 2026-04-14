24d0bcd0c4172671a6e28e1f619afa2a44abf137
brokenMelody13 <440028996@gapps.yrdsb.ca>
2026-04-14 12:00:53 -0400

    Someguy created

--- MyWorld.java
+++ MyWorld.java
@@ -1,7 +1,11 @@
 import greenfoot.*;
 
 public class MyWorld extends World {
-    public MyWorld() {
+    public MyWorld() 
+    {
         super(600, 400, 1);
+        
+        Someguy someguy = new Someguy();
+        addObject(someguy, 100, 100);
     }
 }
---
+++ Someguy.java
@@ -0,0 +1,10 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+public class Someguy extends Actor
+{
+
+    public void act()
+    {
+        if 
+    }
+}
