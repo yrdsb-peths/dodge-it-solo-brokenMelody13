6e781a07d5a3ac5347f3847ce0ca366925360d6d
brokenMelody13 <440028996@gapps.yrdsb.ca>
2026-04-15 10:56:29 -0400

    idk

--- Titleclass.java
+++ Titleclass.java
@@ -3,15 +3,15 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Titleclass extends World
 {
 
-
     public Titleclass()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
-        
+
         showText("Press SPACE to Start", 300, 200);
+        prepare();
     }
-    
+
     public void act()
     {
         if(Greenfoot.isKeyDown("space"))
@@ -20,6 +20,12 @@ public class Titleclass extends World
             Greenfoot.setWorld(gameWorld);
         }
     }
-    
-    
+
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+    }
 }
